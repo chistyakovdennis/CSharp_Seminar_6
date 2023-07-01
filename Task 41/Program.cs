2f17c993e8e39e9134246eeb6389c8575a882670
chistyakovdennis <chistyakov.d@gmail.com>
2023-07-01 10:46:34 +0300

    Решения задач 41 и 43

--- Task 41/Program.cs
+++ Task 41/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите количество чисел, которые хотите ввести:");
+        int M = int.Parse(Console.ReadLine()); // Читаем количество чисел из консоли
+
+        int count = 0; // Счетчик чисел, больших 0
+
+        for (int i = 1; i <= M; i++)
+        {
+            Console.WriteLine("Введите число №" + i + ":");
+            int number = int.Parse(Console.ReadLine()); // Читаем число из консоли
+
+            if (number > 0)
+            {
+                count++;
+            }
+        }
+
+        Console.WriteLine("Количество чисел, больших 0: " + count);
+    }
+}
